8d0b0d9de3635f719e0204427044b801548d5a06
Sakae <81498777+SakaeYasuda@users.noreply.github.com>
2022-03-18 03:23:21 -0300

    Update Untitled2.cpp
    -Void Ler: Chama o comando da leitura dos três números.
    -Void Ordenar: Irá organizar os números recebidos e os classificará em "menor, meio e maior".
    -Int Par: Identifica se o maior dos três números é par ou impar.

--- Untitled2.cpp
+++ Untitled2.cpp
@@ -1,49 +1,170 @@
 #include <stdio.h>
 
-	void Ler(int*n1,int*n2,int*n3);
-	void Par(int num);
+#include <ctype.h>
 
-	int main(){
-   	 	int n1,n2,n3;
-   	 	char opcao;
+ 
 
-    		do{
-	
-    			printf("\nDigite tres numeros inteiros: ");
-   	 			 scanf("%d %d %d", &n1, &n2, &n3);
-   	 			 
-   	 			
-				 if (n1 <= n2 && n2 <= n3)
-    				{
-      					printf("A ordem crescente: %d %d %d\n", n1, n2, n3);
-    				}
-				  		else if (n1 <= n3 && n3 <= n2)
-				    {
-				      printf("A ordem crescente: %d %d %d\n", n1, n3, n2);
-				    }
-				  		else if (n2 <= n1 && n1 <= n3)
-				    {
-				      printf("A ordem crescente: %d %d %d\n", n2, n1, n3);
-				    }
-				  		else if (n2 <= n3 && n3 <= n1) 
-				    {
-				      printf("A ordem crescente: %d %d %d\n", n2, n3, n1);
-				    }
-				  		else if (n3 <= n1 && n1 <= n2) 
-				    {
-				      printf("A ordem crescente: %d %d %d\n", n3, n1, n2);
-				    }
-				  		else  
-				    {
-				      printf("A ordem crescente: %d %d %d\n", n3, n2, n1);
-					}
-					printf("Deseja continuar s/n: \n");
-					fflush(stdin);
-        			scanf(" %c", &opcao);
-    				}while(opcao=='s','S');		
-	
-		
-	}
+void Ler (int *n1, int *n2,int *n3);
+
+void Ordenar (int *menor, int *meio, int *maior);
+
+int    Par (int num);
+
+ 
+
+int main (void) {
+
+            int    num1, num2, num3, par;
+
+            char opcao;
+
+           
+
+            do {   
+
+                        Ler (&num1, &num2, &num3); 
+
+                        Ordenar (&num1, &num2, &num3);
+
+                        par = Par(num3);
+
+ 
+
+                        if (par) {
+
+                                    printf ("Os numeros ordenados sao: %d %d %d (par)\n", num1, num2, num3);
+
+                        } else {
+
+                                    printf ("Os numeros ordenados sao: %d %d %d (impar)\n", num1, num2, num3);         
+
+                        }
+
+                       
+
+                        printf ("Deseja continuar S/N: \n");
+
+                        fflush (stdin);
+
+                        scanf ("%c", &opcao);
+
+                        opcao = toupper(opcao);
+
+            } while (opcao!='N');
+
+ 
+
+            return 0;      
+
+}
+
+ 
+
+void Ler (int *n1, int *n2, int *n3)
+
+{
+
+            printf ("Digite tres numeros inteiros: ");
+
+            scanf ("%d %d %d", n1, n2, n3);          
+
+}
+
+ 
+
+void Ordenar (int* menor, int* meio, int* maior)
+
+{
+
+            int n1 = *menor,
+
+            n2 = *meio,
+
+            n3 = *maior;    
+
+ 
+//n1 sendo o maior.
+            if (n1 >= n2 && n1 >= n3) {
+
+                                    *maior = n1;
+
+                                    if (n2 < n3) {
+
+                                                *menor = n2;
+
+                                                *meio = n3;
+
+                                    } else {
+
+                                                *menor = n3;
+
+                                                *meio = n2;
+
+                                    }
+
+                       
+//n2 sendo o maior.
+            } else if (n2 >= n1 && n2 >= n3) {
+
+                                    *maior = n2;
+
+                                    if (n1 < n3) {
+
+                                               *menor = n1;
+
+                                               *meio = n3;
+
+                                    } else {
+
+                                               *menor = n3;
+
+                                               *meio = n1;
+
+                                    }
 
 
+//n3 sendo o maior.
+            } else if (n3 >= n1 && n3 >= n2) {
 
+                                    *maior = n3;
+
+                                    if (n2 < n1) {
+
+                                                *menor = n2;
+
+                                                *meio = n1;
+
+                                    } else  {
+
+                                                *menor = n1;
+
+                                                *meio = n2;
+
+                                    }
+
+            }
+
+}
+
+ 
+
+int Par (int num) {
+
+            int resposta;
+
+ 
+ 
+            if (num % 2 == 0) {
+
+
+                        resposta = 1;  // verdadeiro demonstra par.
+
+            } else {
+
+
+                        resposta = 0;  // falso demonstra impar.
+
+            }
+
+            return resposta;
+}
